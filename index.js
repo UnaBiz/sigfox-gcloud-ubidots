0a851472ded21550989ad611b4865d73d4b4fb43
lupyuen-unabiz <ly.lee@unabiz.com>
2017-06-04 16:32:11 +0800

    Getting variables from Ubidots

--- index.js
+++ index.js
@@ -14,7 +14,9 @@ if (process.env.FUNCTION_NAME) {
   require('@google-cloud/trace-agent').start();
   require('@google-cloud/debug-agent').start();
 }
-const sgcloud = require('sigfox-gcloud');
+const sgcloud = require('sigfox-gcloud'); //  sigfox-gcloud Framework
+const ubidots = require('ubidots');       //  Ubidots API
+const config = require('./config.json');  //  Ubidots API Key
 
 //  End Common Declarations
 //  //////////////////////////////////////////////////////////////////////////////////////////
@@ -22,11 +24,104 @@ const sgcloud = require('sigfox-gcloud');
 //  //////////////////////////////////////////////////////////////////////////////////////////
 //  Begin Message Processing Code
 
+const apiKey = config['ubidots-api-key'];
+if (!apiKey || apiKey.indexOf('YOUR_') === 0) {
+  throw new Error('ubidots-api-key is missing from config.json');
+}
+let client = null;  //  Ubidots API client.
+
+function debug(res) {
+  //  Debug the result of a promise.  Return the same promise to the next in chain.
+  console.log(JSON.stringify({ res }, null, 2));
+  debugger;
+  return res;
+}
+
+function promisfy(func) {
+  //  Convert the callback-style function in func and return as a promise.
+  return new Promise((resolve, reject) =>
+    func((err, res) => (err ? reject(err) : resolve(res))));
+}
+
+function init(req) {
+  //  This function is called to initialise the Ubidots API client.
+  //  If already initialised, quit.  Returns a promise for the client.
+  if (client) return Promise.resolve(client);
+  const datasourceName = '???';
+  const variableName = '???';
+  const value = 123;
+  let allDatasources = null;
+  let datasource = null;
+  let allVariables = null;
+  let variable = null;
+  let details = null;
+
+  //  Create the Ubidots API client and authenticate with Ubidots.
+  client = ubidots.createClient(apiKey);
+  return promisfy(client.auth)
+    .then(debug)
+    .then(res => promisfy(client.getDatasources))
+    .then(debug)
+    .then(res => { allDatasources = res; })
+    .then(res => client.getDatasource(datasourceName))
+    .then(debug)
+    .then(res => { datasource = res; })
+    .then(res => promisfy(datasource.getVariables))
+    .then(debug)
+    .then(res => { allVariables = res; })
+    .then(res => client.getVariable(variableName))
+    .then(debug)
+    .then(res => { variable = res; })
+    .then(res => promisfy(datasource.getDetails))
+    .then(debug)
+    .then(res => { details = res; })
+    .then(res => promisfy(v.getValues))
+    .then(debug)
+    .then(res => { values = res; })
+    .then(res => v.saveValue(value))
+    .then(debug)
+    .catch((error) => { throw error; });
+
+  /*
+   client.auth(() => {
+    this.getDatasources((err, data) => {
+      console.log(data.results);
+      debugger;
+    });
+
+    const ds = this.getDatasource('xxxxxxxx');
+
+    ds.getVariables((err, data) => {
+      console.log(data.results);
+    });
+
+    ds.getDetails((err, details) => {
+      console.log(details);
+    });
+
+    const v = this.getVariable('xxxxxxx');
+
+    v.getDetails((err, details) => {
+      console.log(details);
+    });
+
+    v.saveValue(22);
+
+    v.getValues((err, data) => {
+      console.log(data.results);
+    });
+    */
+
+  });
+}
+init({});
+
 function task(req, device, body, msg) {
   //  The task for this Google Cloud Function: Send the body of the
   //  Sigfox message to Ubidots by calling the Ubidots API.
-  return processMessage(req, body)
-  //  Return the message with the body updated.
+  //  return processMessage(req, body)
+  return Promise.resolve(body) //
+    //  Return the message with the body updated.
     .then(updatedBody => Object.assign({}, msg, { body: updatedBody }))
     .catch((error) => { throw error; });
 }
