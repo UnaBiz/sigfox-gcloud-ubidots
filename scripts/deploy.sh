81bb0f7978a61e7c7b17cfa0d90f287b2cc33de2
lupyuen-unabiz <ly.lee@unabiz.com>
2017-10-09 03:10:29 +0800

    Updated deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,5 +4,6 @@ name=sendToUbidots
 localpath=.
 trigger=--trigger-topic
 topic=sigfox.types.${name}
+export options="--memory=1024MB --timeout=500"
 
 ./scripts/functiondeploy.sh ${name}   ${localpath} ${trigger} ${topic}
